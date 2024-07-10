ea068fce0ffceee6425de55c94ba77004e3e5645
Agzam <agzamasadullaev@gmail.com>
2024-07-10 18:26:51 +0500

    initial changes

--- app/src/main/java/com/bionickhand/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/bionickhand/vknewsclient/MainActivity.kt
@@ -8,13 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.bionickhand.vknewsclient.ui.theme.PostCard
@@ -25,58 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Test()
+            VkNewsClientTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp),
+                ) {
+                    PostCard()
+                }
+            }
         }
     }
-}
-
-@Composable
-fun Test(){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Example3()
-    }
-}
-
-@Composable
-fun Example1(){
-    OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "Hello world")
-    }
-}
-
-@Composable
-fun Example2(){
-    TextField(
-        value = "Value",
-        onValueChange = {},
-        label = { Text(text = "Label")}
-    )
-}
-
-@Composable
-fun Example3(){
-    AlertDialog(
-        onDismissRequest = {},
-        confirmButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Yes"
-            )
-        },
-        title = {
-            Text(text = "Are you sure?")
-        },
-        text = {
-            Text(text = "Dou you want delete this file?")
-        },
-        dismissButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "No"
-            )
-        }
-    )
 }
