4a4c2d8915b77fa25fd50d31acf8669488105e0c
Agzam <agzamasadullaev@gmail.com>
2024-09-05 23:22:23 +0500

    realize changing likes

--- app/src/main/java/com/bionickhand/vknewsclient/presentation/news/NewsFeedViewModel.kt
+++ app/src/main/java/com/bionickhand/vknewsclient/presentation/news/NewsFeedViewModel.kt
@@ -4,14 +4,10 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bionickhand.vknewsclient.data.mapper.NewsFeedMapper
-import com.bionickhand.vknewsclient.data.network.ApiFactory
+import com.bionickhand.vknewsclient.data.repository.NewsFeedRepository
 import com.bionickhand.vknewsclient.domain.FeedPost
 import com.bionickhand.vknewsclient.domain.StatisticItem
-import com.vk.api.sdk.VKPreferencesKeyValueStorage
-import com.vk.api.sdk.auth.VKAccessToken
 import kotlinx.coroutines.launch
 
 class NewsFeedViewModel(application: Application) : AndroidViewModel(application) {
@@ -21,19 +17,23 @@ class NewsFeedViewModel(application: Application) : AndroidViewModel(application
     private val _screenState = MutableLiveData<NewsFeedScreenState>(initialState)
     val screenState: LiveData<NewsFeedScreenState> = _screenState
 
+    private val repository = NewsFeedRepository(application)
+
     init {
         loadRecommendations()
     }
 
-    private val mapper = NewsFeedMapper()
-
     private fun loadRecommendations(){
         viewModelScope.launch {
-            val storage = VKPreferencesKeyValueStorage(getApplication())
-            val token = VKAccessToken.restore(storage) ?: return@launch
-            val response = ApiFactory.apiService.loadRecommendations(token.accessToken)
-            val feedPosts = mapper.mapResponseToPosts(response)
+            val feedPosts = repository.loadRecommendations()
             _screenState.value = NewsFeedScreenState.PostsState(posts = feedPosts)
+        }
+    }
+
+    fun changeLikeStatus(feedPost: FeedPost){
+        viewModelScope.launch {
+            repository.changeLikeStatus(feedPost)
+            _screenState.value = NewsFeedScreenState.PostsState(posts = repository.feedPosts)
         }
     }
 
