3dab52fa1ec444e316a6b83befc07daad9895e5f
Agzam <agzamasadullaev@gmail.com>
2024-09-09 22:32:52 +0500

    realize comments

--- app/src/main/java/com/bionickhand/vknewsclient/domain/CommentPost.kt
+++ app/src/main/java/com/bionickhand/vknewsclient/domain/CommentPost.kt
@@ -1,11 +1,9 @@
 package com.bionickhand.vknewsclient.domain
 
-import com.bionickhand.vknewsclient.R
-
 data class CommentPost(
-    val id: Int,
-    val authorName: String = "Author",
-    val authorAvatarImageId: Int = R.drawable.comment_author_avatar,
-    val commentText: String = "Long comment text",
-    val publishedDate: String = "14:00"
+    val id: Long,
+    val authorName: String,
+    val authorAvatarUrl: String,
+    val commentText: String,
+    val publishedDate: String
 )
